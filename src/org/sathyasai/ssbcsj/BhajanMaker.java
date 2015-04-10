d556b1697490537177aec97b924c6b94e6c2f4b8
rkanadam <raghuram.kanadam@gmail.com>
2015-04-10 13:15:17 -0700

    Adding support for multiple bhajans

--- src/org/sathyasai/ssbcsj/BhajanMaker.java
+++ src/org/sathyasai/ssbcsj/BhajanMaker.java
@@ -2,6 +2,7 @@ package org.sathyasai.ssbcsj;
 
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -31,17 +32,20 @@ public class BhajanMaker extends HttpServlet {
 
 		final String json = request.getParameter("bhajans");
 
+		final MappingIterator<Bhajan> iterator = new ObjectMapper().reader(
+				Bhajan.class).readValues(json);
+		final List<Bhajan> bhajans = iterator.readAll();
+
 		final XMLSlideShow templatePresentation = new XMLSlideShow(request
 				.getSession().getServletContext()
 				.getResourceAsStream("/WEB-INF/poi.pptx"));
 		final XSLFSlide template = templatePresentation.getSlides()[0];
 		final XMLSlideShow newPresentation = new XMLSlideShow();
 
-		final MappingIterator<Bhajan> iterator = new ObjectMapper().reader(
-				Bhajan.class).readValues(json);
-		while (iterator.hasNext()) {
-			final Bhajan bhajan = iterator.next();
-			XSLFSlide slide = newPresentation.createSlide();
+		for (int i = 0, len = bhajans.size(); i < len; ++i) {
+
+			final Bhajan bhajan = bhajans.get(i);
+			final XSLFSlide slide = newPresentation.createSlide();
 			slide.importContent(template);
 
 			final Iterator<XSLFShape> shapes = slide.iterator();
@@ -55,6 +59,23 @@ public class BhajanMaker extends HttpServlet {
 			((XSLFAutoShape) shapes.next()).getTextParagraphs().get(0)
 					.getTextRuns().get(0).setText(bhajan.getScale());
 
+			if (i + 1 == len) {
+				((XSLFAutoShape) shapes.next()).getTextParagraphs().get(0)
+						.getTextRuns().get(0).setText("");
+				((XSLFAutoShape) shapes.next()).getTextParagraphs().get(0)
+						.getTextRuns().get(0).setText("");
+			} else {
+				final Bhajan nextBhajan = bhajans.get(i + 1);
+				((XSLFAutoShape) shapes.next()).getTextParagraphs().get(0)
+						.getTextRuns().get(0).setText(nextBhajan.getScale());
+				
+				String firstLineOfNextBhajan = nextBhajan.getLyrics().split(
+						"\n")[0];
+				firstLineOfNextBhajan = firstLineOfNextBhajan.substring(0,
+						Math.min(firstLineOfNextBhajan.length(), 25));
+				((XSLFAutoShape) shapes.next()).getTextParagraphs().get(0)
+						.getTextRuns().get(0).setText(firstLineOfNextBhajan);
+			}
 		}
 
 		response.setContentType("application/vnd.ms-ppt");
